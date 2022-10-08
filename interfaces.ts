87681cdc0bb4f5dbfc514f8abddaeae94a0819dc
Duygu Eroğlu <45760467+duygueroglu@users.noreply.github.com>
2022-10-08 14:25:44 +0300

    Update interfaces.ts

--- interfaces.ts
+++ interfaces.ts
@@ -1,14 +1,29 @@
-interface QuizData{
-     title:string;
-     subtitle:string;
-     quizId:string;
+interface QuizData {
+     title: string;
+     subtitle: string;
+     quizId: string;
      content: Content[];
-     answer:Answer[];
+     answer: Answer[];
 }
 
-interface Answer{
-     text:string;
-     image:string;
-     alt:string;
-     combination:string[]
+interface Answer {
+     text: string;
+     image: string;
+     alt: string;
+     combination: string[]
 }
+
+interface Content {
+    id: number;
+    text: string;
+    questions: Question[];
+}
+
+interface Question {
+    text: string;
+    image: string;
+    alt: string;
+    credits: string;
+}
+
+export { QuizData, Answer, Content, Question}
